f2a5a4c7f546752868ae777d567899a7859dddee
noviandiana <109836414+noviandiana@users.noreply.github.com>
2023-01-02 00:36:05 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function() {
+
+	$('.owl-carousel').owlCarousel({
+    margin:10,
+    loop:true,
+    autoWidth:true,
+    items:3,
+	autoplay:true,
+    autoplayTimeout:2500,
+    autoplayHoverPause:true
+})
+
+})
